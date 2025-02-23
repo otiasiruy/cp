4251a3450078ffdb80af9de37a63d158b1279199
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-02-23 08:40:22 -0300

    solution

---
+++ leetcode/3462.py
@@ -0,0 +1,18 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
+        rows = []
+        pq = []
+        for i in range(len(grid)):
+            rows.append(grid[i])
+            rows[i].sort(reverse=True)
+        for i in range(len(rows)):
+            for j in range(limits[i]):
+                heapq.heappush(pq, -rows[i][j])
+        sum = 0
+        for i in range(k):
+            sum += (-heapq.heappop(pq))
+        return sum
