6ab90f8530791b7a34b7ac1b126b2d5e97e4f73b
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-03-01 23:20:48 -0300

    solution

---
+++ leetcode/3467.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[i] = 0
+            else:
+                nums[i] = 1
+        nums.sort()
+        return nums
