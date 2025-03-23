4002396a28b4b16e703f1b28388d928c26660cb6
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-03-23 13:42:41 -0300

    solution

---
+++ leetcode/83.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+ class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def helper(node):
+            if not node:
+                return
+
+            if node.next and node.val == node.next.val:
+                node.next = node.next.next
+                helper(node)
+            elif node.next:
+                helper(node.next)
+
+        helper(head)
+        return head
