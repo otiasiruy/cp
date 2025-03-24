5bf3bbcb75b6ca5e3ec90a00944b0196f79f007d
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-03-24 20:14:38 -0300

    solution

---
+++ leetcode/2025_BigTechChallenge/56.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans = []
+        a = intervals[0][0]
+        b = intervals[0][1]
+        for i in intervals:
+            if a < i[0] and b < i[0]:
+                ans.append([a, b])
+                a = i[0]
+                b = i[1]
+            elif a <= i[0] and i[0] <= b <= i[1]:
+                b = i[1]
+            elif i[0] < a < i[1] and b < i[1]:
+                a = i[0]
+                b = i[1]
+        ans.append([a, b])
+        return ans
