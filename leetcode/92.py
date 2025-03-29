98dc2edf979466c11fe1e3380a0c604243a8cd97
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-03-28 21:29:58 -0300

    solution

--- leetcode/92.py
+++ leetcode/92.py
@@ -5,6 +5,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         l_node, r_node = None, None
