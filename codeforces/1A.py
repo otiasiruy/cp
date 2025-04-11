c61536c3b47472ff5b28396f68bf09b652719381
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-04-11 13:09:50 -0400

    solution

---
+++ codeforces/1A.py
@@ -0,0 +1,41 @@
+import math
+import sys
+import threading
+
+# Uncomment these lines for local testing
+#sys.stdin = open('input', 'r')
+#sys.stdout = open('output', 'w')
+
+input = lambda: sys.stdin.readline().rstrip()
+
+def inp():
+    return int(input())
+
+def inlt():
+    return list(map(int, input().split()))
+
+def insr():
+    return list(input())
+
+def insrsplit(c):
+    return list(input().split(c))
+
+def invr():
+    return map(int, input().split())
+
+def solve():
+    s = inlt()
+
+    ans = math.ceil(s[0] / s[2]) * math.ceil(s[1] / s[2])
+    print(ans)
+
+def main():
+    # Set to True if problem has multiple test cases
+    MULTIPLE_TESTS = False
+
+    t = inp() if MULTIPLE_TESTS else 1
+
+    for _ in range(t):
+        solve()
+if __name__ == "__main__":
+    threading.Thread(target=main).start()
