6d260c34b7abfd9c74552933dcde00d69db8dbaa
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-04-25 20:01:06 -0400

    solution

--- leetcode/5.py
+++ leetcode/5.py
@@ -1,29 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def valid(s):
-            l = 0
-            r = len(s) - 1
-            while l <= r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
         n = len(s)
         if n == 1:
             return s
-        dp = [0] * n
-        dp[0] = 1
-        dp[1] = dp[0]
-        ans = s[0]
-        if valid(s[0:2]):
-            dp[1] = 2
-            ans = s[0:2]
-        for i in range(2, n):
-            l = 0
-            while not valid(s[l:i + 1]):
-                l += 1
-            dp[i] = max(dp[i - 1], i - l + 1)
-            if dp[i] > dp[i - 1]:
-                ans = s[l:i + 1]
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        for i in range(n):
+            dp[i][i] = True
+            if len(ans) < len(s[i : i + 1]):
+                ans = s[i : i + 1]
+            if i < n - 1 and s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                if len(ans) < len(s[i : i + 2]):
+                    ans = s[i : i + 2]
+        for j in range(2, n):
+            for i in range(n - 1):
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    if len(ans) < len(s[i : j + 1]):
+                        ans = s[i : j + 1]
         return ans
