4fc41af2e525a031f2d7c3d1ac0365c154028e80
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-04-29 17:13:45 -0400

    solution

--- leetcode/300.py
+++ leetcode/300.py
@@ -1,19 +1,30 @@
-import bisect
 from typing import List
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        def bs(x):
+            l = 0
+            r = len(dp)
+            while l <= r:
+                mid = l + (r - l) // 2
+                if x == dp[mid]:
+                    return mid
+                elif dp[mid] < x:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return l
+
         n = len(nums)
         if n == 1:
             return 1
-        dp = []
-        dp.append(nums[0])
+        dp = [nums[0]]
         for i in range(1, n):
             if nums[i] > dp[len(dp) - 1]:
                 dp.append(nums[i])
             elif nums[i] < dp[len(dp) - 1]:
-                j = bisect.bisect_left(dp, nums[i])
+                j = bs(nums[i])
                 if nums[i] < dp[j]:
                     dp[j] = nums[i]
         return len(dp)
