785c92fe774033cb74d3999e208343d75724209e
otiasiruy <66180842+otiasiruy@users.noreply.github.com>
2025-05-18 16:56:47 -0400

    solution

--- atcoder/B_PowerSocket.py
+++ atcoder/B_PowerSocket.py
@@ -4,9 +4,9 @@ import sys
 from collections import Counter, deque
 from math import ceil
 
-if os.path.exists('input'):
-    sys.stdin = open('input', 'r')
-    sys.stdout = open('output', 'w')
+if os.path.exists('../solution/input'):
+    sys.stdin = open('../solution/input', 'r')
+    sys.stdout = open('../solution/output', 'w')
 
 def solve():
 
